54cc02616067b694681f7ff118335ab09dd25bc4
codeamac06 <codeamac06@gmail.com>
2017-01-12 12:45:59 -0600

    Impresión de tarjeta mejorada

--- tarcre.rb
+++ tarcre.rb
@@ -1,6 +1,6 @@
 def error_message(name, credit_card_type, credit_card_number)
   if credit_card_type == "#{credit_card_type}" && credit_card_number == "#{credit_card_number}"
-    "#{name}, The #{credit_card_type.upcase} Credit Card Number 'xxx-#{credit_card_number[-3]}#{credit_card_number[-2]}#{credit_card_number[-1]}' You Provided is Invalid." 
+   p "#{name}, The #{credit_card_type.upcase} Credit Card Number 'xxx-#{credit_card_number[-3, 3]}' You Provided is Invalid." 
   end
 end
 
