5289a087cdf003b412d23bcea09f0dee87a56592
codeamac06 <codeamac06@gmail.com>
2017-01-12 13:21:31 -0600

    Añadí ejercicio de métodos destructivos

---
+++ metdes.rb
@@ -0,0 +1,8 @@
+first_name = "Rogelio"
+first_name.upcase!
+last_name = "manzano"
+last_name.capitalize!
+mensaje = "Nombre Correcto"
+mensaje.downcase!
+
+puts mensaje if first_name == "ROGELIO" && last_name == "Manzano"
